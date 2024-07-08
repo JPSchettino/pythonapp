5037966b9872bffb5d8704a60404469610bfb758
JPSchettino <schettino.joaopedro@gmail.com>
2024-07-08 15:54:18 -0300

    att port

--- app.py
+++ app.py
@@ -8,7 +8,5 @@ from rotas.teste import *
 from rotas.estrutura import *
 
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run_server(host='0.0.0.0', port=80)
